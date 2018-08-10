efd39bab6b32141a62d5fbd4af1fd9d143c5ad30
uta666666 <slow-life-sr20@hotmail.co.jp>
2018-08-11 01:35:22 +0900

    とりあえず

---
+++ TicTacToe/Behavior/PanelClickBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace TicTacToe.Behavior
+{
+    public class PanelClickBehavior : Behavior<Button>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.Click += AssociatedObject_Click;
+        }
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.Click -= AssociatedObject_Click;
+        }
+
+        private void AssociatedObject_Click(object sender, RoutedEventArgs e)
+        {
+            AssociatedObject.Content = "○";
+        }
+    }
+}
---
+++ TicTacToe/Utilities/CommonDefinisions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Utilities
+{
+    public enum GridItem
+    {
+        Circle,
+        Cross
+    }
+
+    public static class GridItemExtension
+    {
+        public static string ToString(this GridItem item)
+        {
+            switch (item)
+            {
+                case GridItem.Circle:
+                    return "○";
+                case GridItem.Cross:
+                    return "✕";
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+}
