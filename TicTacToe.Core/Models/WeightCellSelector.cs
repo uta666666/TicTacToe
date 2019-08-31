6b28b57082a546119842267ecc3b36b7788b9e5c
uta666666 <slow-life-sr20@hotmail.co.jp>
2019-08-31 09:41:07 +0900

    リセットボタンの位置を修正　勝負がついたときに線を引きたいけどまだ

--- TicTacToe.Core/Models/WeightCellSelector.cs
+++ TicTacToe.Core/Models/WeightCellSelector.cs
@@ -47,56 +47,6 @@ namespace TicTacToe.Core.Models
          };
 
 
-        //private void ChangeWeight()
-        //{
-        //    var corner1 = new List<Point>()
-        //    {
-        //        new Point(0, 0),
-        //        new Point(1, 0),
-        //        new Point(0, 1)
-        //    };
-        //    var corner2 = new List<Point>()
-        //    {
-        //        new Point(2, 1),
-        //        new Point(2, 2),
-        //        new Point(1, 2)
-        //    };
-        //    var corner3 = new List<Point>()
-        //    {
-        //        new Point(1, 0),
-        //        new Point(2, 0),
-        //        new Point(2, 1)
-        //    };
-        //    var corner4 = new List<Point>()
-        //    {
-        //        new Point(0, 1),
-        //        new Point(0, 2),
-        //        new Point(1, 2)
-        //    };
-
-        //    var selfCells = _board.GetCells(_selfType);
-        //    var nonSelfCells = _board.GetCells(_nonSelfType);
-        //    if (selfCells.Count() == 1)
-        //    {
-        //        if (selfCells.Single().X == 0 && selfCells.Single().Y == 0)
-        //        {
-        //            if (corner3.Any(c => c.X == nonSelfCells.Single().X && c.Y == nonSelfCells.Single().Y))
-        //            {
-
-        //            }
-        //        }
-        //    }
-
-        //    if (nonSelfCells.Any(c => c.Y == 0 && c.X == 0 ||
-        //                              c.Y == 0 && c.X == 2 ||
-        //                              c.Y == 2 && c.X == 0 ||
-        //                              c.Y == 2 && c.X == 2))
-        //    {
-
-        //    }
-        //}
-
-
         public Point? Select(IEnumerable<Point> cells)
         {
             var selfCells = _board.GetCells(_selfType);
